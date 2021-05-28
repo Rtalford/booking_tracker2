6fef9a1339c9e8948c23ab0b3ea1609deb88cd95
Reynelle <Rtalford13@gmail.com>
2021-05-28 09:25:28 -0400

    created controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'flights/index'
+  get 'flights/new'
+  get 'flights/edit'
+  get 'flights/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 
