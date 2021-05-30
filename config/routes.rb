63b90cd5d1ce6c48d5af3534f3d28a1e99931a55
Reynelle <Rtalford13@gmail.com>
2021-05-29 20:58:10 -0400

    created form

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   
   resources :users 
   resources :airlines
+  resources :flights
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
