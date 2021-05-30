1ac7d7f84e72e90bdc5af766f51137f9c0a76325
Reynelle <Rtalford13@gmail.com>
2021-05-30 10:46:18 -0400

    updates

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -4,10 +4,11 @@ class FlightsController < ApplicationController
   end
 
   def new
-    @flights = Flight.new
+    @flight = Flight.new
   end
 
   def create
+    @flight = 
   end 
 
   def show
@@ -21,5 +22,11 @@ class FlightsController < ApplicationController
 
   def destroy
   end
+
+  private 
+
+  def flight_params
+    params.require(:flight).permit(:depart_date, :return_date, :airline_id, :user_id)
+  end 
 
 end
