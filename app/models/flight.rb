b508fc258591bb38ec5e4ec13079f4f04f141a7f
Reynelle <Rtalford13@gmail.com>
2021-05-31 10:33:12 -0400

    validations

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,4 +1,10 @@
 class Flight < ApplicationRecord
   belongs_to :user
   belongs_to :airline
+
+
+  validates :depart_date, presence: :true 
+  validates :return_date, presence: :true 
+  validates :airline, presence: :true
+
 end
