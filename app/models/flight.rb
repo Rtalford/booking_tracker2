53654c839279143f02193d6b765b2366eee14f01
Reynelle <Rtalford13@gmail.com>
2021-06-02 20:22:51 -0400

    updates

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -2,7 +2,7 @@ class Flight < ApplicationRecord
   belongs_to :user
   belongs_to :airline
 
-
+  
   
   validates :depart_date, presence: :true 
   validates :return_date, presence: :true 
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   
   resources :users 
-  resources :airlines
-  resources :flights
+  
+  resources :airlines do
+   resources :flights
+  end 
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
