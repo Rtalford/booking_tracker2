754d20d3e4ba4013022e74fe532edba6c07450d5
Reynelle <Rtalford13@gmail.com>
2021-06-08 09:13:32 -0400

    updates

--- app/controllers/airlines_controller.rb
+++ app/controllers/airlines_controller.rb
@@ -1,2 +1,48 @@
 class AirlinesController < ApplicationController
+    def index
+        @airlines = Airline.all
+      end
+    
+      def new
+        @airlines = Airline.new
+      end
+    
+      def create
+        @airline = airline.new(user_airline)
+        if @airline.save
+          redirect_to user_path(@user)
+        else 
+          render :new
+        end 
+      end
+    
+      def show
+        @airline = Airline.find_by_id(params[:id])
+      end
+    
+      def edit
+        @airline = Airline.find_by_id(params[:id])
+      end
+    
+      def update
+        @airline = airline.find(params[:id])
+        if @airline.update(params)
+          redirect_to user_path
+        else
+          render :edit
+        end 
+      end
+    
+      def destroy
+        @airline = Airline.find_by_id(params[:id])
+        @airline.delete 
+        redirect_to user_path
+      end
+    
+      
+      private 
+    
+      def airline_params
+        params.require(:airline).permit(:name)
+      end 
 end
